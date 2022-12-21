aed2493b71e6cb0310d3d561f8d5192a91b05812
Chris Co <coswarm@gmail.com>
2022-12-21 19:11:46 +0300

    add ListNode to _tester

--- __tests__/_tester.test.js
+++ __tests__/_tester.test.js
@@ -1,7 +1,10 @@
 import tester from '../_tester.js'
 
 describe(`${tester.name || '_tester'}()`, () => {
-  test(``, () => {
-    expect(tester('')).toEqual('')
+  const args = [
+    '',
+  ]
+  test(`${args}`, () => {
+    expect(tester(...args)).toEqual('')
   })
 })
--- _tester.js
+++ _tester.js
@@ -1,5 +1,47 @@
 // TEMPORARY TEST FILE =============================================================================
+/**
+ * @param {string} s
+ * @return {string}
+ */
+export default function _tester (s) {
+  return s
+}
 
-export default function _tester (v) {
-  return v
+// HELPERS -----------------------------------------------------------------------------------------
+
+// Often used in leetcode.com tests
+function ListNode (val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+/**
+ * Construct a ListNode instance from array of numbers
+ * @param {number[]} array
+ * @param {boolean} [reverse] - whether to reverse the order of numbers
+ * @returns {ListNode} listNode
+ */
+function createListNode (array, reverse = false) {
+  let r, next
+  if (!reverse) array.reverse()
+  for (const n of array) {
+    r = next = new ListNode(n, next)
+  }
+  return r
+}
+
+/**
+ * Get ListNode values as array of numbers
+ * @param {ListNode} listNode
+ * @returns {number[]} array of numbers
+ */
+function getListNodeArray (listNode) {
+  if (listNode.val == null) return []
+  let r = [listNode.val]
+  let list = listNode
+  while (list.next) {
+    r.push(list.next.val)
+    list.next = list.next.next
+  }
+  return r
 }
